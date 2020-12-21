0458867eea5c194ce8cb5775e2c17c99ddbb6a8b
Tim Johnson <tim@itstimjohnson.com>
2020-12-20 21:19:23 -0500

    cleaned up backend code, and commented

--- main.go
+++ main.go
@@ -1,28 +1,23 @@
 package main
 
 import (
-	"bufio"
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"regexp"
 	"strconv"
-	"strings"
-	"sync"
-
-	"github.com/blevesearch/bleve"
 )
 
 func main() {
 	searcher := Searcher{}
+	fmt.Println("Loading in the works of Shakespeare!..")
 	err := searcher.Load("completeworks.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println("Loaded!")
 
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fs)
@@ -39,34 +34,6 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-const (
-	TITLE_MARKER = "🙂"
-	MAX_PAGE_LEN = 30
-)
-
-type Searcher struct {
-	Index bleve.Index
-}
-
-type ShakeDocument struct {
-	Work string `json:"work"`
-	Text string `json:"text"`
-	Lines int `json:"lines"`
-}
-
-type SearchResult struct {
-	ID string `json:"id"`
-	Fragments []string `json:"fragments"`
-	ShakeDocument
-}
-
-type SearchResults struct {
-	Total	int 			`json:"total"`
-	Page	int 			`json:"page"`
-	Length	int				`json:"length"`
-	Results []SearchResult	`json:"results"`
 }
 
 // handleSearch returns a search handler with the given searcher, allowing for easy dependency injection.
@@ -122,127 +89,4 @@ func handleSearch(searcher Searcher) func(w http.ResponseWriter, r *http.Request
 		w.Header().Set("Content-Type", "application/json")
 		w.Write(buf.Bytes())
 	}
-}
-
-func (s *Searcher) Load(filename string) error {
-	docMapping := bleve.NewDocumentStaticMapping()
-	docMapping.AddFieldMappingsAt("work", bleve.NewTextFieldMapping())
-	docMapping.AddFieldMappingsAt("text", bleve.NewTextFieldMapping())
-	linesMapping := bleve.NewNumericFieldMapping()
-	linesMapping.Index = false
-	docMapping.AddFieldMappingsAt("lines", linesMapping)
-
-	idxMapping := bleve.NewIndexMapping()
-	idxMapping.DefaultMapping = docMapping
-
-	// To preserve the original benefit, this index should not be persisted and remain only in memory
-	index, err := bleve.NewMemOnly(idxMapping)
-	if err != nil {
-		return err
-	}
-	s.Index = index
-	
-	dat, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return fmt.Errorf("Load: %w", err)
-	}
-	works := strings.Split(string(dat), TITLE_MARKER)[1:]
-
-	// Free the memory
-	dat = nil
-
-	var wg sync.WaitGroup
-	wg.Add(len(works))
-	headerBlockRe := regexp.MustCompile(`\n[A-Z.,1-9 ]+\n`)
-	nonEmptyNewlineRe := regexp.MustCompile(`\n\s*`)
-	batches := make([]*bleve.Batch, len(works))
-	for i, w := range works {
-		go func(i int, w string) {
-			defer wg.Done()
-			batches[i] = index.NewBatch()
-			_, title, _ := bufio.ScanLines([]byte(w), false)
-			// if err != nil {
-			// 	return err
-			// }
-			
-			indices := headerBlockRe.FindAllStringIndex(w, -1)
-			startIdx := 0
-			j := 0
-			for _, idx := range indices {
-				// Strip off the starting newline
-				block := w[startIdx:idx[0]-1]
-				startIdx = idx[0] + 1
-				batches[i].Index(fmt.Sprintf("%s-%v", title, j), ShakeDocument{
-					Work: string(title),
-					Text: block,
-					Lines: len(nonEmptyNewlineRe.FindAllStringIndex(block, -1)) + 1,
-				})
-				j++
-			}
-			block := w[startIdx:]
-			batches[i].Index(fmt.Sprintf("%s-%v", title, j), ShakeDocument{
-				Work: string(title),
-				Text: block,
-				Lines: len(nonEmptyNewlineRe.FindAllStringIndex(block, -1)) + 1,
-			})
-		}(i, w)
-	}
-	wg.Wait()
-
-	combined := index.NewBatch()
-	for _, b := range batches {
-		combined.Merge(b)
-	}
-	return index.Batch(combined)
-}
-
-func (s *Searcher) Search(query string, page, length int) (SearchResults, error) {
-	// Construct the query- the match query has fuzziness embedded
-	q := bleve.NewMatchQuery(query)
-	q.SetField("text")
-	search := bleve.NewSearchRequestOptions(q, length, page*length, false)
-	search.Fields = []string{"*"}
-	search.Highlight = bleve.NewHighlightWithStyle("html") // html.Name
-	r, err := s.Index.Search(search)
-	if err != nil {
-		return SearchResults{}, err
-	}
-
-	if r.Total == 0 {
-		q.SetFuzziness(1)
-		r, err = s.Index.Search(search)
-		if err != nil {
-			return SearchResults{}, err
-		}
-
-		if r.Total == 0 {
-			q.SetFuzziness(2)
-			r, err = s.Index.Search(search)
-			if err != nil {
-				return SearchResults{}, err
-			}
-		}
-	}
-
-	// Convert the results to the ShakeSearch result form
-	results := make([]SearchResult, len(r.Hits))
-	for i, h := range r.Hits {
-		temp, err := json.Marshal(h.Fields)
-		if err != nil {
-			return SearchResults{}, err
-		}
-		err = json.Unmarshal(temp, &results[i])
-		if err != nil {
-			return SearchResults{}, err
-		}
-		results[i].ID = h.ID
-		results[i].Fragments = h.Fragments["text"]
-	}
-
-	return SearchResults{
-		Total: int(r.Total),
-		Page: page,
-		Length: length,
-		Results: results,
-	}, nil
 }
---
+++ searcher.go
@@ -0,0 +1,185 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"regexp"
+	"strings"
+	"sync"
+
+	"github.com/blevesearch/bleve"
+)
+
+const (
+	// TitleMarker is the marker that directly precedes the title of a work
+	TitleMarker = "🙂"
+
+	// MaxPageLength is the maximum number of lines to return as a page
+	MaxPageLength = 30
+)
+
+// Searcher stores the intelligently separated text of Shakespeare for searching
+type Searcher struct {
+	Index bleve.Index
+}
+
+// ShakeDocument is a single document from the works of Shakespeare, which should
+// be a single "block" of text preceded by a header
+type ShakeDocument struct {
+	Work string `json:"work"`
+	Text string `json:"text"`
+	Lines int `json:"lines"`
+}
+
+// SearchResult is a single result from the search function
+type SearchResult struct {
+	ID string `json:"id"`
+	Fragments []string `json:"fragments"`
+	ShakeDocument
+}
+
+// SearchResults is the collection of results from the search function
+type SearchResults struct {
+	Total	int 			`json:"total"`
+	Page	int 			`json:"page"`
+	Length	int				`json:"length"`
+	Results []SearchResult	`json:"results"`
+}
+
+// Load takes the given file, processes it, and allows it to be searched with
+// the Search() function. It is tailored to the Shakespeare works, where each
+// is preceded by the constant utf-8 character "🙂". The documents that will
+// be produced are each full "blocks" of text, which at the moment are sections
+// of text that are preceded by a header of a character name, act, etc.
+func (s *Searcher) Load(filename string) error {
+	// Create the index meant to store the ShakeDocument structs. Static mapping ensures
+	// that only the data we want is present and stored. To preserve the original format,
+	// this index is created in-memory
+	docMapping := bleve.NewDocumentStaticMapping()
+	docMapping.AddFieldMappingsAt("work", bleve.NewTextFieldMapping())
+	docMapping.AddFieldMappingsAt("text", bleve.NewTextFieldMapping())
+	linesMapping := bleve.NewNumericFieldMapping()
+	linesMapping.Index = false
+	docMapping.AddFieldMappingsAt("lines", linesMapping)
+
+	idxMapping := bleve.NewIndexMapping()
+	idxMapping.DefaultMapping = docMapping
+
+	index, err := bleve.NewMemOnly(idxMapping)
+	if err != nil {
+		return err
+	}
+	s.Index = index
+	
+	// Read the file into memory and split it into individual works based on the 🙂 character
+	// This has room for improvement in memory efficiency, but it works for the size of
+	// Shakespeare so it's fine to do it this way.
+	dat, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return fmt.Errorf("Load: %w", err)
+	}
+	works := strings.Split(string(dat), TitleMarker)[1:]
+	dat = nil  // Free the memory
+
+	// On each work, concurrently generate documents that are separated by "block", or a
+	// group of text that has a header line.
+	var wg sync.WaitGroup
+	wg.Add(len(works))
+	headerBlockRe := regexp.MustCompile(`\n[A-Z.,1-9 ]+\n`)
+	nonEmptyNewlineRe := regexp.MustCompile(`\n\s*`)
+	batches := make([]*bleve.Batch, len(works))
+	for i, w := range works {
+		go func(i int, w string) {
+			defer wg.Done()
+			batches[i] = index.NewBatch()
+			_, title, _ := bufio.ScanLines([]byte(w), false)
+			// if err != nil {
+			// 	return err
+			// }
+			
+			indices := headerBlockRe.FindAllStringIndex(w, -1)
+			startIdx := 0
+			j := 0
+			for _, idx := range indices {
+				// Strip off the starting newline
+				block := w[startIdx:idx[0]-1]
+				startIdx = idx[0] + 1
+				batches[i].Index(fmt.Sprintf("%s-%v", title, j), ShakeDocument{
+					Work: string(title),
+					Text: block,
+					Lines: len(nonEmptyNewlineRe.FindAllStringIndex(block, -1)) + 1,
+				})
+				j++
+			}
+			block := w[startIdx:]
+			batches[i].Index(fmt.Sprintf("%s-%v", title, j), ShakeDocument{
+				Work: string(title),
+				Text: block,
+				Lines: len(nonEmptyNewlineRe.FindAllStringIndex(block, -1)) + 1,
+			})
+		}(i, w)
+	}
+	wg.Wait()
+
+	// I don't believe that the index is thread safe, so batch everything at once.
+	combined := index.NewBatch()
+	for _, b := range batches {
+		combined.Merge(b)
+	}
+	return index.Batch(combined)
+}
+
+// Search searches through the internal contents for the given query, returning
+// "length" results, starting from the "page"th page of length results.
+func (s *Searcher) Search(query string, page, length int) (SearchResults, error) {
+	// Construct the query- the match query has fuzziness embedded
+	q := bleve.NewMatchQuery(query)
+	q.SetField("text")
+	search := bleve.NewSearchRequestOptions(q, length, page*length, false)
+	search.Fields = []string{"*"}
+	search.Highlight = bleve.NewHighlightWithStyle("html") // html.Name
+	r, err := s.Index.Search(search)
+	if err != nil {
+		return SearchResults{}, err
+	}
+
+	if r.Total == 0 {
+		q.SetFuzziness(1)
+		r, err = s.Index.Search(search)
+		if err != nil {
+			return SearchResults{}, err
+		}
+
+		if r.Total == 0 {
+			q.SetFuzziness(2)
+			r, err = s.Index.Search(search)
+			if err != nil {
+				return SearchResults{}, err
+			}
+		}
+	}
+
+	// Convert the results to the ShakeSearch result form
+	results := make([]SearchResult, len(r.Hits))
+	for i, h := range r.Hits {
+		temp, err := json.Marshal(h.Fields)
+		if err != nil {
+			return SearchResults{}, err
+		}
+		err = json.Unmarshal(temp, &results[i])
+		if err != nil {
+			return SearchResults{}, err
+		}
+		results[i].ID = h.ID
+		results[i].Fragments = h.Fragments["text"]
+	}
+
+	return SearchResults{
+		Total: int(r.Total),
+		Page: page,
+		Length: length,
+		Results: results,
+	}, nil
+}
